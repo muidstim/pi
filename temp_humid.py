65f964a8e8df63dd7f61c0ec8877a33e12dcdd8e
muidstim <tim.gir@mahidol.edu>
2018-11-26 14:13:01 +0700

    Update temp_humid.py

--- temp_humid.py
+++ temp_humid.py
@@ -1,6 +1,9 @@
+# sudo pip3 install Adafruit_DHT
+
 import Adafruit_DHT
+pin = 4
 
 while True:
-  humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+  humidity, temperature = Adafruit_DHT.read_retry(11, pin)
   print ("Temperature:", temperature)
   print ("Humidity:", humidity)
