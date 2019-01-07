b7204aaee3e77871b2b56807f9716e1054fb7c63
muidstim <tim.gir@mahidol.edu>
2019-01-07 12:40:25 +0700

    Update touchsensor.py

--- touchsensor.py
+++ touchsensor.py
@@ -1,13 +1,27 @@
+import RPi.GPIO as #BLANK#
+import #BLANK#
+
+#BLANK# - set the pin
+
 def setup():
-  print("Setting Up Touch Sensor and LED")
+  print("Setting Up Touch Sensor and LED on pin", TouchPin)
   GPIO.setmode(GPIO.BCM)
   GPIO.setup(TouchPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
   
-  
+def detectTouch():
+  while True:
+    time.sleep(0.2)
+    
+    if GPIO.input(#BLANK#) == GPIO.LOW:
+      print ('...led off')
+    else:
+      print ('led on...')
+      
 try:
   print("Raspberry Pi Touch Sensor Sample Program")
-  setup()
+  #BLANK#  
   print("Setup Complete")
-  loop()
+  detectTouch()
 finally:
+  #BLANK#
   print("done")
